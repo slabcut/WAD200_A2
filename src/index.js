ca84ce9344a869cd97ec17a55bdcfeb4230e097b
slabcut <dave@holycow.com.au>
2022-05-12 16:19:13 +1000

    2 files modified, 4 deleted

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import "./styles/index.css";
 
-ReactDOM.render(
-  <h1>Hello World</h1> ,
-  document.getElementById("root")
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <h1> Hello React </h1>
 );
